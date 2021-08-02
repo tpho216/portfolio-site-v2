e4601e3a9c1dada2aac992f5c9de1a57689cea4a
tpho216 <tpho216@gmail.com>
2021-08-03 02:03:06 +1000

    update service worker registration

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import * as ReactDOM from "react-dom";
 import {App} from "./App";
 import {About} from "./components/about/About";
 
+import {DebugHelper} from "../Util/DebugHelper";
+import firebase from "firebase/app";
+
 require('./index.css');
 
 ReactDOM.render(
@@ -10,4 +13,26 @@ ReactDOM.render(
     document.getElementById("output")
 )
 
+let messaging = null;
+
+
+const registerFirebaseServiceWorker = async () => {
+    messaging = firebase.messaging();
+    //Avoid registering token multiple times
+    let currentToken = await messaging.getToken();
+    DebugHelper('current token for messaging client: hidden');
+    if (currentToken === null) {
+        const swRegistration = await navigator.serviceWorker.register("../firebase-messaging-sw.js");
+        DebugHelper("Registration successful, scope is:" + swRegistration.scope);
+        currentToken = await messaging.getToken({vapidKey: process.env.REACT_APP_webPushCertificatePublicVapidKey, serviceWorkerRegistration : swRegistration });
+        if (currentToken) {
+            DebugHelper('current token for client: hidden');
+        }
+        else {
+            DebugHelper('current token for client: ' + currentToken); //should be null
+        }
+    }
+}
+
+registerFirebaseServiceWorker();
 
